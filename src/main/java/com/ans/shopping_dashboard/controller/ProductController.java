f559f079e3aa2a2386533025f5aeb26961d985e8
Justyna Jachowicz <jachowiczjustyna10@gmail.com>
2023-10-15 13:35:50 +0200

    dodanie/edytowanie produktów

--- src/main/java/com/ans/shopping_dashboard/controller/ProductController.java
+++ src/main/java/com/ans/shopping_dashboard/controller/ProductController.java
@@ -1,12 +1,10 @@
 package com.ans.shopping_dashboard.controller;
 
+import com.ans.shopping_dashboard.model.Product;
 import com.ans.shopping_dashboard.repository.ProductListRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin")
@@ -24,15 +22,27 @@ public class ProductController {
         return "products";
     }
 
-    public void addProduct() {
-
+    @GetMapping("/product/add")
+    public String addProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "addProduct";
     }
 
-    public void editProduct() {
-
+    @PostMapping("/product/add")
+    public String postProduct(@ModelAttribute("product") Product product) {
+        productListRepository.save(product);
+        return "redirect:/admin/products";
     }
 
-    @RequestMapping(value ="/product/delete/{id}", method = {RequestMethod.DELETE, RequestMethod.GET})
+    @GetMapping("/product/edit/{id}")
+    public String editProduct(@PathVariable Long id, Model model) {
+        var product = productListRepository.findById(id);
+        model.addAttribute("product", product);
+
+        return "addProduct";
+    }
+
+    @RequestMapping(value = "/product/delete/{id}", method = {RequestMethod.DELETE, RequestMethod.GET})
     public String deleteProduct(@PathVariable Long id) {
         productListRepository.deleteById(id);
         return "redirect:/admin/products";
