79c7ec2bb912edbffaaab70cd08b4ff4dadb40bf
justynajachowicz <jachowiczjustyna10@gmail.com>
2024-12-03 00:23:26 +0100

    SD-05: Dodanie plików backendu aplikacji

---
+++ Shopping-Dashboard/sd-backend/src/main/java/com/example/shopping_dashboard_backend/ShoppingDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping_dashboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingDashboardBackendApplication.class, args);
+	}
+
+}
