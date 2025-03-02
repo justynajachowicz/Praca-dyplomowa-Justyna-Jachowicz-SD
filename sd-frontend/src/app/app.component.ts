01d76088e69f621b553b5ed7373153a2676fef8b
justynajachowicz <116719009+justynajachowicz@users.noreply.github.com>
2025-03-02 21:31:06 +0100

    usunięcie importu

--- sd-frontend/src/app/app.component.ts
+++ sd-frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 
