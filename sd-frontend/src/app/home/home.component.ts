7a9d0b4ddd04cb58f38d16d1ced88f141142ffcb
justynajachowicz <jachowiczjustyna10@gmail.com>
2025-03-30 13:59:11 +0200

    ++sladjer

--- sd-frontend/src/app/home/home.component.ts
+++ sd-frontend/src/app/home/home.component.ts
@@ -1,18 +1,43 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import {FormsModule} from "@angular/forms";
+import { CommonModule } from '@angular/common'; // 👈 DODAJ TO!
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [SlickCarouselModule, FormsModule, CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
 export class HomeComponent {
     constructor(private router: Router) {}
 
+    slides = [
+        { img: "assets/images/obrazek1.png" },
+        { img: "assets/images/obrazek2.png" },
+        { img: "assets/images/obrazek3.png" },
+        { img: "assets/images/obrazek4.png" }
+    ];
+
+    slideConfig = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: true,
+        infinite: true,
+        centerMode: false,
+        variableWidth: false,
+        adaptiveHeight: false
+    };
+
+    cities = ['Nowy Sącz', 'Kraków', 'Gdańsk', 'Wrocław', 'Poznań'];
+    selectedCity = this.cities[0];
 
     goToAddPurchase() {
+        console.log(`Planowanie zakupów dla miasta: ${this.selectedCity}`);
         this.router.navigate(['/add-purchase']);
-
     }
 }
